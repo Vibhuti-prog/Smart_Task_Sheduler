b5bae3b598473511104341c97092926cf87f2c13
Vibhuti-prog <vibhutisingh2005@gmail.com>
2026-07-27 15:57:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,24 @@
 //     exclude: ['lucide-react'],
 //   },
 // });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/smart-task-scheduler/',
+
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smart-task-scheduler/',
 
   optimizeDeps: {
     exclude: ['lucide-react'],
